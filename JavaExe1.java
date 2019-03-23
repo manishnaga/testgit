a16e0c186b2a7d593c67fa62835b74a96fe9839d
manishnaga <mailtomaniya92@gmail.com>
2019-03-23 13:52:23 +0530

    adding first java file

---
+++ JavaExe1.java
@@ -0,0 +1,21 @@
+package Exe1.module1;
+
+public class JavaExe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+
+	}
+
+}
